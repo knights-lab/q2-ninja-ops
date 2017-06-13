a0a51ed536e000422aeb262e25c8aa123e482e07
Benjamin Hillmann <bhillmann@outlook.com>
2017-06-13 15:44:55 -0500

    Updated the init.

--- q2_ninja_ops/__init__.py
+++ q2_ninja_ops/__init__.py
@@ -3,3 +3,26 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
+import json
+import os.path
+import tempfile
+import subprocess
+
+import biom
+import skbio.io
+
+# TODO handle wrapping external applications better, this copies/parses a lot
+# of data unnecessarily
+def pick_reference_otus(sequences):
+    # TODO update when https://github.com/biocore/qiime2/issues/12 is resolved
+    with tempfile.TemporaryDirectory(prefix='q2ninja-ops-temp-') as tempdir:
+        input_fasta_filepath = os.path.join(tempdir, 'input-sequences.fasta')
+        output_directory = os.path.join(tempdir, 'ninja-ops-output')
+        skbio.io.write(sequences, format='fasta', into=input_fasta_filepath)
+
+        completed_process = subprocess.run(
+            ['ninja.py', '-i', input_fasta_filepath, '-o', output_directory],
+            check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        with open(os.path.join(output_directory, 'ninja_otutable.biom')) as fh:
+            return biom.Table.from_json(json.load(fh))
