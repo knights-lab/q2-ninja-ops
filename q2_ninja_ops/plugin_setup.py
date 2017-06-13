7514955139c0c09ef16575260ef9fe2865e37d99
Benjamin Hillmann <bhillmann@outlook.com>
2017-06-13 15:43:25 -0500

    Setup the plugin.

--- q2_ninja_ops/plugin_setup.py
+++ q2_ninja_ops/plugin_setup.py
@@ -1,49 +1,23 @@
-# ----------------------------------------------------------------------------
-# Copyright (c) 2016--, QIIME development team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file LICENSE, distributed with this software.
-# ----------------------------------------------------------------------------
-
 from qiime.plugin import Plugin, Int
 
-import q2_ninja_ops
-# from feature_table.artifact_types import (
-    # FeatureTable, Frequency, RelativeFrequency, PresenceAbsence)
+import q2ninja_ops
+from q2ninja_ops.artifact_types import SequenceData, DNA
+from feature_table.artifact_types import FeatureTable, Frequency
 
 plugin = Plugin(
     name='ninja-ops',
-    version=q2_ninja_ops.__version__,
-    website='http://ninja-ops.ninja',
-    package='q2_ninja_ops'
+    version=q2ninja_ops.__version__,
+    website='https://github.com/knights-lab/q2-ninja-ops',
+    package='q2ninja_ops'
 )
 
-# TODO create decorator for promoting functions to workflows. This info would
-# be moved to the decorator calls.
-# plugin.register_function(
-#     function=feature_table.rarefy,
-#     # TODO use more restrictive primitive type for `depth`
-#     inputs={'table': FeatureTable[Frequency], 'depth': Int},
-#     outputs=[('rarefied_table', FeatureTable[Frequency])],
-#     name='Rarefaction',
-#     doc="Let's rarefy!"
-# )
-#
-# plugin.register_function(
-#     function=feature_table.presence_absence,
-#     inputs={'table': FeatureTable[~PresenceAbsence]},
-#     outputs=[('presence_absence_table', FeatureTable[PresenceAbsence])],
-#     name='Convert to presence/absence',
-#     doc="Let's convert to presence/absence!"
-# )
-#
-# plugin.register_function(
-#     function=feature_table.relative_frequency,
-#     inputs={'table': FeatureTable[Frequency]},
-#     outputs=[('relative_frequency_table', FeatureTable[RelativeFrequency])],
-#     name='Convert to relative frequencies',
-#     doc="Let's convert to relative frequencies!"
-# )
-#
-# plugin.register_workflow('workflows/summarize.md')
+plugin.register_function(
+    function=q2ninja_ops.pick_reference_otus,
+    # TODO make this accept other types of sequence data as input
+    inputs={'sequences': SequenceData[DNA]},
+    outputs=[('feature_table', FeatureTable[Frequency])],
+    name='Pick closed reference OTUs',
+    # TODO write better docs
+    doc="Runs NINJA-OPS on sequence data to produce a closed reference OTU "
+        "table"
+)
