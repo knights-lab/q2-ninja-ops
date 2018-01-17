1662c9552c91eb2042690d9e8a0fe501ecbd6aa4
bhillmann <bhillmann@outlook.com>
2018-01-17 15:56:37 -0600

    Ready for Jai to take a look/

--- q2_ninja_ops/_database.py
+++ q2_ninja_ops/_database.py
@@ -6,9 +6,12 @@
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
 
+import os
+
 from q2_types.feature_data import DNAFASTAFormat
 
 from q2_ninja_ops import NinjaOpsDBDirFmt
+from q2_ninja_ops._util import run_command
 
 
 def build_database(reference_sequences: DNAFASTAFormat) -> NinjaOpsDBDirFmt:
@@ -19,6 +22,23 @@ def build_database(reference_sequences: DNAFASTAFormat) -> NinjaOpsDBDirFmt:
     database = NinjaOpsDBDirFmt()
     database_dir = str(database)
 
-    # TODO run commands to build NINJA-OPS database inside `database_dir`
+    output_prefix = "db"
+
+    # Run commands to build NINJA-OPS database inside `database_dir`
+    # NINJA prep the database
+    ninja_prep_cmd = ['ninja_prep', ref_filepath, output_prefix]
+    run_command(ninja_prep_cmd)
+
+    # Build the Bowtie2 from ninja prep
+    bt2_build_cmd = ['bowtie2-build-s', output_prefix + ".fa", output_prefix]
+    run_command(bt2_build_cmd)
+
+    # Set the required files for db
+    output_suffixes = (".1.bt2", ".2.bt2", ".3.bt2", ".4.bt2", ".rev.1.bt2", ".rev.2.bt2", ".db", ".tcf")
+    output_files = (output_prefix + _ for _ in output_suffixes)
+
+    # Move all files to database dir from QIIME2 nebula working dir
+    for file in output_files:
+        os.rename(file, os.path.join(database_dir, file))
 
     return database
---
+++ q2_ninja_ops/tests/test_database.py
@@ -0,0 +1,9 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2017, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+#TODO: I really don't know how to make a unittest case for this plugin within the QIIME2 paradigm
